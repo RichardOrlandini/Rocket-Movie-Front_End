f06c3a80228c28351331aa84c59cf14c42e2fe03
RichardOrlandini <richardorlandini09@gmail.com>
2023-03-13 14:55:49 -0300

    Create  Tag,  Note and finish home page

--- Front-End/src/pages/Home/index.jsx
+++ Front-End/src/pages/Home/index.jsx
@@ -1,16 +1,45 @@
 
+import { Button } from "../../components/Button";
 import { Header } from "../../components/Header";
-import { Container } from "./styled";
+import { Section} from "../../components/Section";
+import { Note} from "../../components/Note";
 
 
+
+import { Container ,Content , NewNote} from "./styled";
 
 
 export function Home(){
 
     return (
           <Container>
+
             <Header/>
+
+            <NewNote>
+            <h1>Meus filmes</h1>
+              
+              <Button title="+ Adicionar Filme">
+              </Button>
+            </NewNote>
             
+            <Content>
+
+              <Section >
+                <Note 
+                  data={{
+                    title: 'Interestellar',
+                    tags: [
+                      {id: '1', name: 'react'},
+                      {id: '2', name: 'node'},
+                    ]
+                  }}
+                />
+              </Section>
+
+
+            </Content>
+
            </Container>
       
     )
