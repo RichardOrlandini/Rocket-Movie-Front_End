9ef2c320af723dc461696b526d3af72bb99ecdfd
RichardOrlandini <richardorlandini09@gmail.com>
2023-03-15 11:25:40 -0300

    creation functions in profile file

--- Front-End/src/hooks/auth.jsx
+++ Front-End/src/hooks/auth.jsx
@@ -39,8 +39,17 @@ function AuthProvider({ children }){
 		
 	}
 
-	async function updateProfile({ user }){
+	async function updateProfile({ user, avatarFile }){
 		try {
+
+			if (avatarFile){
+				const fileUploadForm = new FormData();
+				fileUploadForm.append("avatar", avatarFile);
+
+				const response = await api.patch("/users/avatar", fileUploadForm);
+				user.avatar = response.data.avatar;
+			}
+
 			await api.put("/users", user);
 
 			localStorage.setItem("@rocktmovie:user", JSON.stringify(user));
